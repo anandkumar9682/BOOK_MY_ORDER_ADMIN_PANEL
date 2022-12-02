414220ad67cc0d1453cbc363569fd1d683394416
Anand Kumar <anandkumar.docs@gmail.com>
2022-12-02 11:09:37 +0530

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (eid, fname, lname,email)
+VALUES (1,'Anand', 'kumar','anandkumar.docs@gmail.com');
+commit;
+INSERT INTO employee (eid, fname, lname,email)
+VALUES (2,'lav', 'rajput','lavrajput9998@gmail.com');
+commit;
